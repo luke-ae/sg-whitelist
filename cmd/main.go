193c7af12bfdcd091979aa26a51da8e7d1bd7dc3
Luke Macauley <lukemacauley@mac.com>
2024-09-01 09:58:25 +0400

    clenaup

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,9 @@ import (
 
 	"github.com/dgraph-io/ristretto"
 	"github.com/go-chi/chi/v5"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		panic("Error loading .env file")
-	}
 
 	r := chi.NewRouter()
 
